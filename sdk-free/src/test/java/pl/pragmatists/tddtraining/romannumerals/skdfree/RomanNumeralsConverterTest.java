d44527944704f0b8ca4ea20b8708fceaf97f44c5
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2015-06-24 12:14:40 +0200

    Add static import and fix gradle build.

--- sdk-free/src/test/java/pl/pragmatists/tddtraining/romannumerals/skdfree/RomanNumeralsConverterTest.java
+++ sdk-free/src/test/java/pl/pragmatists/tddtraining/romannumerals/skdfree/RomanNumeralsConverterTest.java
@@ -3,20 +3,22 @@ package pl.pragmatists.tddtraining.romannumerals.skdfree;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RomanNumeralsConverterTest {
 
     @Test
     public void converts_1_to_I() {
         String result = new RomanNumeralsConverter().arabicToRoman("1");
 
-        Assertions.assertThat(result).isEqualTo("I");
+        assertThat(result).isEqualTo("I");
     }
 
     @Test
     public void converts_2_to_II() {
         String result = new RomanNumeralsConverter().arabicToRoman("2");
 
-        Assertions.assertThat(result).isEqualTo("II");
+        assertThat(result).isEqualTo("II");
     }
 
 }
